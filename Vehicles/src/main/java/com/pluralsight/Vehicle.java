5f932e30c6e0915356b4308c860ba7ed9d06d46a
sabuharb <abuharb.sandy@gmail.com>
2024-10-29 10:45:36 -0500

    initial commit

---
+++ Vehicles/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+
+    // getter and setter for color
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    //
+}
